5374343c0bc2889e67c3425f834da6cbdf0d69c8
KelvinRdz <krodriguez50322@ufide.ac.cr>
2025-03-19 20:18:45 -0600

    Add files via upload

---
+++ quizz/JavaApplication3/src/javaapplication3/semana.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class semana { 
+    
+
+
+    private String diasSemana[] = {"lunes", "Martes", "Miercoles", "jueves", "Viernes", "sabado", "Domingo"};
+    private String productos[] =  {"Arroz", "Coca", "Leche", "atun", "azucar",};
+    private String Ventas[][] = {};
+    
+    /**
+     *
+     * @param productos
+     * @return 
+     * @returnllenar matrices
+     */
+    public  char llenar_arreglo(ventas[i][j] arreglo) {
+         for (int i = 1; i < diasSemana.length; i++) {
+            for (int j = 1; j < productos.length; j++) {
+                char llenar = Integer.parseInt(JOptionPane.showInputDialog("Digite la cantidad de ventas "));
+         return arreglo[i][j];
+            };
+        };
+    };
+    
+
+}
